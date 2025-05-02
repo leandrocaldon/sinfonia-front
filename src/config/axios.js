aae1f6207ccf55cd7583f5190e5f01e0513566d4
leandrocaldon <22380691+leandrocaldon@users.noreply.github.com>
2025-05-01 21:52:23 -0500

    se modifica el axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Configuración base de axios
 const instance = axios.create({
-  baseURL: 'https://back-sinfonia.vercel.app'
+  baseURL: 'https://sinfonia-back.vercel.app/'
 });
 
 export default instance;
