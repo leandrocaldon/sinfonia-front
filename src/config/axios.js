6a5e058c20b1542ca85dc6a25ba4c4ee50a0fd0f
leandrocaldon <22380691+leandrocaldon@users.noreply.github.com>
2025-05-03 08:12:17 -0500

    se configuro axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Configuración base de axios
 const instance = axios.create({
-  baseURL: 'https://sinfonia-back.vercel.app/'
+  baseURL: 'https://sinfonia-back.vercel.app/api/'
 });
 
 export default instance;
+
