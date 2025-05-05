ec7fa1ff4c49618c04324d4c2084a478f8b36dac
leandrocaldon <22380691+leandrocaldon@users.noreply.github.com>
2025-05-04 19:57:47 -0500

    se integró el footer en la app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Products from './pages/Products';
@@ -9,16 +10,19 @@ import AdminContactMessages from './pages/AdminContactMessages';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Products />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admin/contact-messages" element={<AdminContactMessages />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin/contact-messages" element={<AdminContactMessages />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
